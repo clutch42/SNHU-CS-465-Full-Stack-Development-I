56d7cb2b2c5d48e5aa0ef6ea6258cb281762ac56
Brian Engel <clutch_42@yahoo.com>
2024-02-08 15:03:27 -0600

    Module 5

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,6 +1,6 @@
 // .seedgooserc.js
 module.exports = {
-    modelBaseDirectiory: "app_server/database/models", // model directory name
+    modelBaseDirectiory: "app_api/database/models", // model directory name
     models: ["*.js", "!db.js"], // model matcher
     data: "data", // data directory name
     db: "mongodb://127.0.0.1:27017/travlr" // db connection url 
